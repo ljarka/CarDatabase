1f771746429e2f5e9c0eda3ef24f4c2aef41bce1
Lukasz Jarka <lukasz.jarka@allegrogroup.com>
2017-03-27 20:48:59 +0200

    Fragment

--- app/src/main/java/com/example/lukaszjarka/cardatabase/listing/ListingActivity.java
+++ app/src/main/java/com/example/lukaszjarka/cardatabase/listing/ListingActivity.java
@@ -2,18 +2,36 @@ package com.example.lukaszjarka.cardatabase.listing;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import com.example.lukaszjarka.cardatabase.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ListingActivity extends AppCompatActivity {
     private static final String QUERY = "query";
+
+    @BindView(R.id.fragment_container)
+    FrameLayout fragmentContainer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listing);
+        ButterKnife.bind(this);
+
+        String query = getIntent().getStringExtra(QUERY);
+        Fragment fragment = ListingFragment.getInstance(query);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 
     public static Intent createIntent(Context context, String query) {
--- app/src/main/java/com/example/lukaszjarka/cardatabase/listing/ListingFragment.java
+++ app/src/main/java/com/example/lukaszjarka/cardatabase/listing/ListingFragment.java
@@ -1,13 +1,16 @@
 package com.example.lukaszjarka.cardatabase.listing;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.lukaszjarka.cardatabase.MotoDatabaseOpenHelper;
 import com.example.lukaszjarka.cardatabase.R;
 
 import butterknife.BindView;
@@ -15,11 +18,28 @@ import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class ListingFragment extends Fragment {
+    private static final String QUERY_KEY = "query_key";
 
     @BindView(R.id.recyler_view)
     RecyclerView recyclerView;
 
     private Unbinder unbinder;
+
+    private MotoDatabaseOpenHelper openHelper;
+
+    public static Fragment getInstance(String query) {
+        ListingFragment fragment = new ListingFragment();
+        Bundle arguments = new Bundle();
+        arguments.putString(QUERY_KEY, query);
+        fragment.setArguments(arguments);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        openHelper = new MotoDatabaseOpenHelper(getActivity());
+    }
 
     @Nullable
     @Override
@@ -32,7 +52,12 @@ public class ListingFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         unbinder = ButterKnife.bind(this, view);
-
+        String query = getArguments().getString(QUERY_KEY);
+        RecyclerViewCursorAdapter recyclerViewCursorAdapter = new RecyclerViewCursorAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext(),
+                LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(recyclerViewCursorAdapter);
+        recyclerViewCursorAdapter.setCursor(openHelper.searchQuery(query));
     }
 
     @Override
